2927559568ca59209fe994054051bb437f88827b
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2022-04-21 21:56:09 +0700

    smth

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 docker build . -t ivanch1ck/node-mongo
-docker run -p 8000:3000 ivanch1ck/node-mongo
 docker push ivanch1ck/node-mongo
+
+if [[ "$1" == "run" ]]
+then
+  docker run -p 8000:3000 ivanch1ck/node-mongo
+fi
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongo from 'mongodb'
 
 const app = express();
 const port = 3000;
+const mong = new mongo.MongoClient("");
+
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('hello from typescript!');
 });
