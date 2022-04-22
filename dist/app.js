d04b07178b20a94bbdc9b380f1ca2f05f3d6e7da
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2022-04-22 17:53:53 +0700

    new files

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const connection_1 = require("./db/connection");
 const app = (0, express_1.default)();
 const port = 3000;
-(0, connection_1.run)();
 app.get('/', (req, res) => {
-    res.send('hello from typescript!');
+    console.log(`${port}`);
+    const path = __dirname.replace(/dist/, "/resource/views/landing.html");
+    res.sendFile(path);
 });
 app.listen(port).on("error", () => {
     console.log('error while trying listen the port' + port);
--- src/server/db/connection.ts
+++ src/server/db/connection.ts
@@ -1,19 +1,13 @@
 import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
 
-const url = "mongodb+srv://ivan:ba1man_sucks@cluster0.91yu0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const mongoClient: mongoDB.MongoClient = new mongoDB.MongoClient(url);
-
-export async function run() {
-    try {
-        await mongoClient.connect();
-
-        console.log("connected to database");
-    } catch (err: any) {
-        console.log(err);
-    }
-}
-
 export async function connect() {
     dotenv.config();
+    const url = process.env.DB_CONNECTION_STRING;
+
+    if (url !== undefined) {
+        const mongoClient: mongoDB.MongoClient = new mongoDB.MongoClient(url);
+
+    } else
+        throw new URIError("couldn't find connection url");
 }
